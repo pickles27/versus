68c8896c314dba302f4ad3a113ee7a446eec4128
Sarah Pickrahn <spickrahn@gmail.com>
2025-04-11 20:55:41 -0700

    fix build

--- src/app/new-game/page.tsx
+++ src/app/new-game/page.tsx
@@ -1,10 +1,7 @@
-
-export async function NewGame() {
+export default async function NewGame() {
   return (
     <div className="h-screen w-full flex items-center justify-center">
       <h1 className="text-2xl font-bold">New Game</h1>
     </div>
   );
 }
-
-export default NewGame;
